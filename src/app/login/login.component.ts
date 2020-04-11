3e1b8c8e661ce2fc3e52d388a46b0a1fbe59937e
volodymyrrozdolsky <rozdolskyvolodymyr@gmail.com>
2020-04-11 13:16:33 -0400

    added logi, password reset and signup components

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AccountService } from '../service/account.service';
+import { LoadingService } from '../service/loading.service';
+import { AlertType } from '../enum/alert-type.enum';
+import { AlertService } from '../service/alert.service';
+import { User } from '../model/user';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
+  private subscriptions: Subscription[] = [];
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private accountService: AccountService,
+    private loadingService: LoadingService,
+    private alertService: AlertService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    if (this.accountService.isLoggedIn()) {
+      if (this.accountService.redirectUrl) {
+        this.router.navigateByUrl(this.accountService.redirectUrl);
+      } else {
+        this.router.navigateByUrl('/home');
+      }
+    } else {
+      this.router.navigateByUrl('/login');
+    }
   }
 
+  onLogin(user: User): void {
+    this.loadingService.isLoading.next(true);
+    console.log(user);
+    this.subscriptions.push(
+      this.accountService.login(user).subscribe(
+        (response) => {
+          const token: string = response.headers.get('Authorization');
+          this.accountService.saveToken(token);
+          if (this.accountService.redirectUrl) {
+            this.router.navigateByUrl(this.accountService.redirectUrl);
+          } else {
+            this.router.navigateByUrl('/home');
+          }
+          this.loadingService.isLoading.next(false);
+        },
+        (error) => {
+          console.log(error);
+          this.loadingService.isLoading.next(false);
+          this.alertService.showAlert(
+            'Username or password incorrect. Please try again.',
+            AlertType.DANGER
+          );
+        }
+      )
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach((sub) => sub.unsubscribe);
+  }
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,15 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
+import { LoadingService } from '../service/loading.service';
+import { AccountService } from '../service/account.service';
+import { AlertType } from '../enum/alert-type.enum';
+import { AlertService } from '../service/alert.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent implements OnInit, OnDestroy {
+  private subscriptions: Subscription[] = [];
 
-  constructor() { }
+  constructor(
+    private accountService: AccountService,
+    private router: Router,
+    private loadingService: LoadingService,
+    private alertService: AlertService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    if (this.accountService.isLoggedIn()) {
+      if (this.accountService.redirectUrl) {
+        this.router.navigateByUrl(this.accountService.redirectUrl);
+      } else {
+        this.router.navigateByUrl('/home');
+      }
+    } else {
+      this.router.navigateByUrl('/signup');
+    }
   }
 
+  onRegister(user): void {
+    this.loadingService.isLoading.next(true);
+    console.log(user);
+    this.subscriptions.push(
+      this.accountService.register(user).subscribe(
+        (response) => {
+          this.loadingService.isLoading.next(false);
+          this.alertService.showAlert(
+            'You have registered successfully. Please check your email for account details.',
+            AlertType.SUCCESS
+          );
+          console.log(response);
+        },
+        (error: HttpErrorResponse) => {
+          console.log(error);
+          this.loadingService.isLoading.next(false);
+          const errorMsg: string = error.error;
+          if (errorMsg === 'usernameExist') {
+            this.alertService.showAlert(
+              'This username already exists. Please try with a different username',
+              AlertType.DANGER
+            );
+          } else if (errorMsg === 'emailExist') {
+            this.alertService.showAlert(
+              'This email address already exists. Please try with a different email',
+              AlertType.DANGER
+            );
+          } else {
+            this.alertService.showAlert(
+              'Something went wrong. Please try again.',
+              AlertType.DANGER
+            );
+          }
+        }
+      )
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach((sub) => sub.unsubscribe());
+  }
 }
