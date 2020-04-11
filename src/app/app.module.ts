eb1aace96a9767b8eacc6503618f0796babde715
volodymyrrozdolsky <rozdolskyvolodymyr@gmail.com>
2020-04-11 07:23:40 -0400

    initial generated components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    ResetPasswordComponent,
+    HomeComponent,
+    ProfileComponent,
+    NavbarComponent,
+    PostDetailComponent
   ],
   imports: [
     BrowserModule
